921428c47fbd897f4e10f4c3db293e3b49256e2f
QYS <qianyingshuo@hotmail.com>
2015-12-17 09:57:52 +0800

    修复了在kill掉进程后，会出现killed输出的bug
    不能不说，在运维中，我还是一个bash脚本的初学者，这个函数出现了很多我不知道的缺点，虽然最后得以解决，但是我还是不知道具体的解决原理，解决办法的得出也只能说是侥幸。
    有谁对kill命令知道的更加透彻，希望给出意见

--- function.sh
+++ function.sh
@@ -65,6 +65,9 @@ DOTPID=$!;
 # descriper : 在你的程序逻辑执行完毕后，运行这个函数，然后在输出你想输出的东西。
 function loopDotEND() {
 kill -9 $DOTPID;
+# 这特么是玄学，为什么会成为这个样子？用其他的命令来输出，就仍然会有killed输出，用ps -ef 就没有
+# 了，但是只能这么用了。
+ps -ef 
 for ((i=0;i <= $DOTNUM; i++)); do
     echo -ne '\b \b';
 done
--- test.sh
+++ test.sh
@@ -4,7 +4,8 @@ source function.sh
 
 echo "Hello World";
 loopDotBEGIN 15;
-wait $DOTPID
-# sleep 30;
-loopDotEND
+# wait $DOTPID
+sleep 30;
+loopDotEND 1>/dev/null 2>/dev/null
 echo "Well Done";
+sleep 10;
